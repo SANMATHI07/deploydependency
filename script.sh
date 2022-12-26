65c70fb558cd2294548da7702db717782302e797
SANMATHI07 <115545050+SANMATHI07@users.noreply.github.com>
2022-12-26 17:08:33 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
-install awscli
+install_awscli()
 {
-    apt-update -y
-    apt-install awscli -y
+    sudo apt-get update
+   sudo apt-get -y install awscli
     if [ $? -ne 0 ]; then
     echo "awscli not installed"
     fi
+    echo "awscli installed successfully"
 }
-install netcat
+install_netcat()
 {
-    apt-update -y
-    apt-install netcat -y
+   sudo apt-get update
+   sudo apt-get -y install netcat
     if [ $? -ne 0 ]; then
     echo "netcat not installed"
     fi
+    echo "netcat installed successfully"
 }
-install tsh
-{
-    apt-update -y
-    apt-install tsh -y
-    if [ $? -ne 0 ]; then
-    echo "tsh not installed"
-    fi
+install_tsh() 
+{ 
+   sudo apt update -y
+   sudo curl https://apt.releases.teleport.dev/gpg \
+-o /usr/share/keyrings/teleport-archive-keyring.asc
+source /etc/os-release
+echo "deb [signed-by=/usr/share/keyrings/teleport-archive-keyring.asc] \
+https://apt.releases.teleport.dev/${ID?} ${VERSION_CODENAME?} stable/v11" \
+| sudo tee /etc/apt/sources.list.d/teleport.list > /dev/null
+sudo apt-get update
+sudo apt-get install teleport
+if [ $? -ne 0 ]; then
+echo "TSH not installed"
+fi
+echo "TSH installed successfully"
 }
-awscli
-netcat
-tsh
+
+
+install_awscli
+install_netcat
+install_tsh
